02c0dbee1138c2b90ec3437bb0500d969e7e3f03
osman67239121@gmail.com <osman67239121@gmail.com>
2020-02-10 09:48:23 +0300

    add uls to app

---
+++ instagram/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns=[
+    
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^$', views.home, name = 'welcome'),
+    
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
