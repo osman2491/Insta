912b2aee41ee22da58c5ee920ca7de93b108955b
osman67239121@gmail.com <osman67239121@gmail.com>
2020-02-10 09:44:26 +0300

    add image model

--- instagram/models.py
+++ instagram/models.py
@@ -8,3 +8,14 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
    profile_photo = models.ImageField(blank=True) 
    bio = models.TextField()
+
+class Image(models.Model):
+    image_name = models.CharField(max_length =30)
+    image_photo = ImageField(blank=True, manual_crop="")
+    image_caption = models.TextField()
+    image_profile = models.ForeignKey(Profile,on_delete=models.CASCADE, default = 1)
+    image_likes = models.IntegerField(default = 0,null=True)
+    image_comments = models.TextField(blank = True) 
+        
+    def __str__(self):
+        return self.image_name
