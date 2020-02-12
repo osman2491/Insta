2d593f386df944a2ea3ee5d98f58ba9df5578d89
osman67239121@gmail.com <osman67239121@gmail.com>
2020-02-12 10:27:33 +0300

    initialize test

--- instagram/tests.py
+++ instagram/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Image, Profile
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class ImageTestClass(TestCase):
+   # Setup Method
+   def setUp(self):
+      self.profile=Profile(id = 1)
+      self.image_one=Image(image_name='moana',image_caption='By the seashore',image_comments= 'Quality image')
